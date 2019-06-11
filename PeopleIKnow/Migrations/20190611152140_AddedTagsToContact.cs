a0c155757666dfcb8ab853717391b77a11943efb
Lars Richter <github@n-develop.com>
2019-06-11 17:31:39 +0200

    WIP: Implement tags. Added missing migration

---
+++ PeopleIKnow/Migrations/20190611152140_AddedTagsToContact.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PeopleIKnow.Migrations
+{
+    public partial class AddedTagsToContact : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Tags",
+                table: "Contacts",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Tags",
+                table: "Contacts");
+        }
+    }
+}
