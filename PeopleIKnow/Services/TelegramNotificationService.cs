fa25ce232d39ce083d85873eb089fc8999b05a8f
Lars Richter <github@n-develop.com>
2019-06-19 07:55:09 +0200

    Added error handling for notifications

--- PeopleIKnow/Services/TelegramNotificationService.cs
+++ PeopleIKnow/Services/TelegramNotificationService.cs
@@ -1,11 +1,14 @@
+using System;
 using System.Net.Http;
 using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
 namespace PeopleIKnow.Services
 {
     public class TelegramNotificationService : INotificationService
     {
+        private readonly ILogger _logger;
         private readonly NotificationSettings _notificationSettings;
 
         private const string ApiUrl =
@@ -13,8 +16,9 @@ namespace PeopleIKnow.Services
 
         private const string MessageTemplate = "*{0}*\n\n{1}";
 
-        public TelegramNotificationService(IOptions<NotificationSettings> notificationSettings)
+        public TelegramNotificationService(IOptions<NotificationSettings> notificationSettings, ILogger logger)
         {
+            _logger = logger;
             _notificationSettings = notificationSettings.Value;
         }
 
@@ -25,11 +29,18 @@ namespace PeopleIKnow.Services
                 return;
             }
 
-            using (var client = new HttpClient())
+            try
             {
-                var text = string.Format(MessageTemplate, title, message);
-                await client.GetAsync(string.Format(ApiUrl, _notificationSettings,
-                    System.Net.WebUtility.UrlEncode(text)));
+                using (var client = new HttpClient())
+                {
+                    var text = string.Format(MessageTemplate, title, message);
+                    await client.GetAsync(string.Format(ApiUrl, _notificationSettings,
+                        System.Net.WebUtility.UrlEncode(text)));
+                }
+            }
+            catch (Exception e)
+            {
+                _logger.LogWarning(e, $"Notification could not be send.\nTitle: {title}\nMessage: {message}");
             }
         }
     }
