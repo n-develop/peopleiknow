f9bdab563dbcf4bb0e8bc6425aa95b126443c96a
Lars Richter <github@n-develop.com>
2019-07-09 23:27:41 +0200

    WIP working on mobile stuff. Not great, but working for now.

--- PeopleIKnow/wwwroot/js/dashboard.js
+++ PeopleIKnow/wwwroot/js/dashboard.js
@@ -43,9 +43,12 @@ function addContactTeaserClickEvent() {
     contactCards.forEach(function (currentValue, currentIndex, listObj) {
         currentValue.onclick = handleTeaserClick;
     });
+
+    const backButton = document.getElementById("back-button");
+    backButton.onclick = showFeed;
 }
 
-function showFeed() {
+function hideFeed() {
     const feed = document.getElementById("people-feed");
     feed.classList.remove("is-full-mobile");
     feed.classList.add("is-hidden-mobile");
@@ -55,12 +58,22 @@ function showPane() {
     const pane = document.getElementById("people-pane");
     pane.classList.remove("is-hidden-mobile");
     pane.classList.add("is-full-mobile");
+
+    hideFeed();
+
+    const backButton = document.getElementById("back-button");
+    backButton.classList.remove("is-hidden-mobile");
 }
 
-function hideFeed() {
+function showFeed() {
     const feed = document.getElementById("people-feed");
     feed.classList.remove("is-hidden-mobile");
     feed.classList.add("is-full-mobile");
+
+    hidePane();
+
+    const backButton = document.getElementById("back-button");
+    backButton.classList.add("is-hidden-mobile");
 }
 
 function hidePane() {
@@ -74,9 +87,34 @@ function handleTeaserClick(element) {
     fetch("/Dashboard/Details/" + id)
         .then(updatePane);
 
-    showFeed();
     showPane();
 }
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    // Get all "navbar-burger" elements
+    const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+
+    // Check if there are any navbar burgers
+    if ($navbarBurgers.length > 0) {
+
+        // Add a click event on each of them
+        $navbarBurgers.forEach(el => {
+            el.addEventListener('click', () => {
+
+                // Get the target from the "data-target" attribute
+                const target = el.dataset.target;
+                const $target = document.getElementById(target);
+
+                // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+                el.classList.toggle('is-active');
+                $target.classList.toggle('is-active');
+
+            });
+        });
+    }
+
+});
 
 addContactTeaserClickEvent();
 
