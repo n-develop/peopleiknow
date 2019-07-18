93d91606f61ef8f9d3284948739e55cf0882cbe8
Lars Richter <github@n-develop.com>
2019-07-18 07:51:45 +0200

    Added first tests for Add method of ContactController

---
+++ PeopleIKnow.UnitTests/ContactControllerTests/AddTests.cs
@@ -0,0 +1,53 @@
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using NSubstitute;
+using PeopleIKnow.Models;
+using Xunit;
+
+namespace PeopleIKnow.UnitTests.ContactControllerTests
+{
+    public class AddTests : BaseContactControllerTests
+    {
+        [Fact]
+        public void Get_ReturnsView()
+        {
+            // Arrange
+            var controller = CreateController();
+
+            // Act
+            var actionResult = controller.Add();
+
+            // Assert
+            actionResult.Should().NotBeNull();
+            actionResult.Should().BeOfType<ViewResult>();
+        }
+
+        [Fact]
+        public void Post_ReceivesContactWithId_ReturnsBadRequest()
+        {
+            // Arrange
+            var controller = CreateController();
+
+            // Act
+            var actionResult = controller.Add(new Contact {Id = 3});
+
+            // Assert
+            actionResult.Should().NotBeNull();
+            actionResult.Should().BeOfType<BadRequestResult>();
+        }
+
+        [Fact]
+        public void Post_ContactCouldNotBeCreated_ReturnsBadRequest()
+        {
+            // Arrange
+            _contactRepository.AddContact(Arg.Any<Contact>()).Returns(NullContact.GetInstance());
+            var controller = CreateController();
+
+            // Act
+            var actionResult = controller.Add(new Contact());
+
+            // Assert
+            actionResult.Should().BeOfType<BadRequestResult>();
+        }
+    }
+}
