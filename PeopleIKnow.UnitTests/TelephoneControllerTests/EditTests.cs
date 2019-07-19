9efe7ddc0e9c5dd18c16cd62a5f8de0d35f3bcd8
Lars Richter <github@n-develop.com>
2019-07-19 17:53:59 +0200

    Added more tests for TelephoneController. For actions Edit and Delete

--- PeopleIKnow.UnitTests/TelephoneControllerTests/EditTests.cs
+++ PeopleIKnow.UnitTests/TelephoneControllerTests/EditTests.cs
@@ -2,6 +2,7 @@ using System;
 using FluentAssertions;
 using Microsoft.AspNetCore.Mvc;
 using NSubstitute;
+using NSubstitute.Core;
 using PeopleIKnow.Models;
 using Xunit;
 
@@ -10,7 +11,7 @@ namespace PeopleIKnow.UnitTests.TelephoneControllerTests
     public class EditTests : BaseTelephoneControllerTests
     {
         [Fact]
-        public void ReceivesInvalidId_ReturnsNotFound()
+        public void Get_ReceivesInvalidId_ReturnsNotFound()
         {
             // Arrange
             var controller = CreateController();
@@ -23,7 +24,7 @@ namespace PeopleIKnow.UnitTests.TelephoneControllerTests
         }
 
         [Fact]
-        public void CannotFindContactById_ReturnsNotFound()
+        public void Get_CannotFindContactById_ReturnsNotFound()
         {
             // Arrange
             _contactRepository.GetTelephoneNumberById(2).Returns(NullTelephoneNumber.GetInstance());
@@ -37,7 +38,7 @@ namespace PeopleIKnow.UnitTests.TelephoneControllerTests
         }
 
         [Fact]
-        public void ReceivesValidId_ReturnsView()
+        public void Get_ReceivesValidId_ReturnsView()
         {
             // Arrange
             _contactRepository.GetTelephoneNumberById(2).Returns(new TelephoneNumber {Id = 2});
@@ -51,7 +52,7 @@ namespace PeopleIKnow.UnitTests.TelephoneControllerTests
         }
 
         [Fact]
-        public void ReceivesValidId_ReturnsModelWithId()
+        public void Get_ReceivesValidId_ReturnsModelWithId()
         {
             // Arrange
             _contactRepository.GetTelephoneNumberById(2).Returns(new TelephoneNumber {Id = 2});
@@ -64,6 +65,46 @@ namespace PeopleIKnow.UnitTests.TelephoneControllerTests
             var model = (actionResult as ViewResult).Model;
             model.Should().BeOfType<TelephoneNumber>();
             (model as TelephoneNumber).Id.Should().Be(2);
+        }
+
+        [Fact]
+        public void Post_ReceivesNullAsModel_ReturnsBadRequest()
+        {
+            // Arrange
+            var controller = CreateController();
+
+            // Act
+            var actionResult = controller.Edit(null);
+
+            // Assert
+            actionResult.Should().BeOfType<BadRequestResult>();
+        }
+
+        [Fact]
+        public void Post_ReceivesValidMode_ReturnsRedirect()
+        {
+            // Arrange
+            var controller = CreateController();
+
+            // Act
+            var actionResult = controller.Edit(new TelephoneNumber());
+
+            // Assert
+            actionResult.Should().BeOfType<RedirectToActionResult>();
+        }
+
+        [Fact]
+        public void Post_ReceivesValidModel_ReturnsRedirectToDetails()
+        {
+            // Arrange
+            var controller = CreateController();
+
+            // Act
+            var actionResult = controller.Edit(new TelephoneNumber()) as RedirectToActionResult;
+
+            // Assert
+            actionResult.ActionName.Should().Be("Details");
+            actionResult.ControllerName.Should().Be("Dashboard");
         }
     }
 }
