397b7a722e356dc45406f358a9f2ac38dc37acaa
Lars Richter <github@n-develop.com>
2021-05-16 01:43:10 +0200

    Added a first test

---
+++ PeopleIKnow.UiTests/CreateContactTests.cs
@@ -0,0 +1,59 @@
+using System;
+using System.Threading;
+using FluentAssertions;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using Xunit;
+
+namespace PeopleIKnow.UiTests
+{
+    public class CreateContactTests : IDisposable
+    {
+        private readonly IWebDriver _driver;
+
+        public CreateContactTests()
+        {
+            _driver = new ChromeDriver();
+        }
+
+        public void Dispose()
+        {
+            _driver.Quit();
+            _driver.Dispose();
+        }
+
+        [Fact]
+        public void NewContactIsShownInTheFeed()
+        {
+            _driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(10);
+            _driver.Manage().Timeouts().AsynchronousJavaScript = TimeSpan.FromSeconds(10);
+            Login();
+            Thread.Sleep(1000);
+
+            AddNewContact();
+            Thread.Sleep(1000);
+
+            var feed = _driver.FindElement(By.Id("people-feed"));
+            var teasers = feed.FindElements(By.ClassName("card"));
+            teasers.Should().Contain(c => c.Text == "Harry James Potter");
+        }
+
+        private void AddNewContact()
+        {
+            _driver.FindElement(By.Id("add-button")).Click();
+            Thread.Sleep(1000);
+            _driver.FindElement(By.Id("Firstname")).SendKeys("Harry");
+            _driver.FindElement(By.Id("Middlename")).SendKeys("James");
+            _driver.FindElement(By.Id("Lastname")).SendKeys("Potter");
+            _driver.FindElement(By.Id("save-button")).Click();
+        }
+
+        private void Login()
+        {
+            _driver.Navigate().GoToUrl("https://localhost:5001/");
+            _driver.FindElement(By.Id("Input_Email")).SendKeys("test@test.com");
+            _driver.FindElement(By.Id("Input_Password")).SendKeys("Test1234.");
+            _driver.FindElement(By.Id("Submit_Button")).Click();
+        }
+    }
+}
