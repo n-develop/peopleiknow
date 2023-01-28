097bc1a6809b48f70d1408501cc51c15c4a03d9a
Lars Richter <lars.richter@parshipgroup.com>
2023-01-28 23:02:43 +0100

    Almost done with localization

--- PeopleIKnow/Models/GiftType.cs
+++ PeopleIKnow/Models/GiftType.cs
@@ -1,9 +1,14 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace PeopleIKnow.Models
 {
     public enum GiftType
     {
+        [Display(Name = "received")]
         Received,
+        [Display(Name = "given")]
         Given,
+        [Display(Name = "idea")]
         Idea
     }
 }
